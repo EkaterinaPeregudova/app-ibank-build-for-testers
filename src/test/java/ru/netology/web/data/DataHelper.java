f4596b33ff0bd66fe096760b3e5a991a6a9dde6d
katerina-kis <katerina-kis@yandex.ru>
2021-05-04 16:52:52 +0300

    v3

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -1,6 +1,11 @@
 package ru.netology.web.data;
 
 import lombok.Value;
+import lombok.val;
+import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.TransferPage;
+
+import static ru.netology.web.page.DashboardPage.*;
 
 public class DataHelper {
     private DataHelper() {}
@@ -26,5 +31,46 @@ public class DataHelper {
 
     public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
         return new VerificationCode("12345");
+    }
+//
+    @Value
+    public static class Cards {
+        private String cardNumber;
+        private String id;
+        private int cardBalance;
+    }
+
+    public static Cards firstCard() {
+        return new Cards(
+                "5559 0000 0000 0001",
+                "[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']",
+                10000);
+    }
+
+    public static Cards secondCard() {
+        return new Cards(
+                "5559 0000 0000 0002",
+                "[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']",
+                10000);
+    }
+
+    public static void resetBalance(DashboardPage dashboardPage, int firstCardBalance, int secondCardBalance) {
+
+        if ((firstCardBalance == firstCard().getCardBalance()) && (secondCardBalance == secondCard().getCardBalance())) {
+            return;
+        }
+
+        if (firstCardBalance < firstCard().getCardBalance()) {
+            String differ = Integer.toString(firstCard().getCardBalance() - firstCardBalance);
+            val transferPage = dashboardPage.transfer(firstCard().getId());
+            transferPage.transferAmount(differ, secondCard().getCardNumber().strip());
+
+        }
+        if (secondCardBalance < secondCard().getCardBalance()) {
+            String differ = Integer.toString(secondCard().getCardBalance() - secondCardBalance);
+            val transferPage = dashboardPage.transfer(secondCard().getId());
+            transferPage.transferAmount(differ, firstCard().getCardNumber().strip());
+
+        }
     }
 }
--- src/test/java/ru/netology/web/page/DashboardPege.java
+++ src/test/java/ru/netology/web/page/DashboardPege.java
@@ -1,14 +1,34 @@
 package ru.netology.web.page;
 
 import com.codeborne.selenide.SelenideElement;
+import lombok.val;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class DashboardPege {
     private SelenideElement heading = $("[data-test-id=dashboard]");
+    private final String balanceStart = "баланс: \n";
+    private final String balanceFinish = "\n р. ";
+    private final String transferButton = " [data-test-id=action-deposit]";
 
     public DashboardPage() {
         heading.shouldBe(visible);
+    }
+    public int getCardBalance(String id) {
+        String text = $(id).getOwnText();
+        return extractBalance(text);
+    }
+
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+
+    public TransferPage transfer(String toCard) {
+        $(toCard + transferButton).click();
+        return new TransferPage();
     }
 }
