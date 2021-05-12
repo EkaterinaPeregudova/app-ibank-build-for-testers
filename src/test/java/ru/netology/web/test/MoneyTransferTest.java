b0f9d0817ed869911101c8c41b3dce24f8ceab86
katerina-kis <katerina-kis@yandex.ru>
2021-05-12 13:31:28 +0300

    в первом тесте баланс уходит в минус

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,40 +1,87 @@
 package ru.netology.web.test;
 
 import lombok.val;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import ru.netology.web.page.*;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.web.data.DataHelper.*;
 
-public class MoneyTransferTest {
-    @Test
-    void shouldTransferMoneyBetweenOwnCardsV1() {
+
+class MoneyTransferTest {
+    private DashboardPage dashboardPage;
+
+    int transferAmount = 5000;
+
+
+    @BeforeEach
+    void setUp() {
+
         open("http://localhost:9999");
-        val loginPage = new LoginPageV1();
-//    val loginPage = open("http://localhost:9999", LoginPageV1.class);
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
+        val login = new LoginPage();
+        dashboardPage = login.validLogin(getAuthInfo()).validVerify(getVerificationCodeFor(getAuthInfo()));
+        val balanceFirstCard = dashboardPage.getCardBalance(firstCard().getId());
+        val balanceSecondCard = dashboardPage.getCardBalance(secondCard().getId());
+        resetBalance(dashboardPage, balanceFirstCard, balanceSecondCard);
+
+    }
+
+
+    @Test
+    void shouldTransferMoneyFromFirstToSecond() {
+
+        val transferPage = dashboardPage.transfer(secondCard().getId());
+        transferPage.transferAmount(Integer.toString(transferAmount), firstCard().getCardNumber().strip());
+
+        val actualFirst = dashboardPage.getCardBalance(firstCard().getId());
+        val expectedFirst = firstCard().getCardBalance() - transferAmount;
+        assertEquals(expectedFirst, actualFirst);
+
+        val actualSecond = dashboardPage.getCardBalance(secondCard().getId());
+        val expectedSecond = secondCard().getCardBalance() + transferAmount;
+        assertEquals(expectedSecond, actualSecond);
+
     }
 
     @Test
-    void shouldTransferMoneyBetweenOwnCardsV2() {
-        open("http://localhost:9999");
-        val loginPage = new LoginPageV2();
-//    val loginPage = open("http://localhost:9999", LoginPageV2.class);
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
+    void shouldTransferMoneyFromSecondToFirst() {
+
+        val transferPage = dashboardPage.transfer(firstCard().getId());
+        transferPage.transferAmount(Integer.toString(transferAmount), secondCard().getCardNumber().strip());
+
+        val actualFirst = dashboardPage.getCardBalance(firstCard().getId());
+        val expectedFirst = firstCard().getCardBalance() + transferAmount;
+        assertEquals(expectedFirst, actualFirst);
+
+        val actualSecond = dashboardPage.getCardBalance(secondCard().getId());
+        val expectedSecond = secondCard().getCardBalance() - transferAmount;
+        assertEquals(expectedSecond, actualSecond);
+    }
+
+    private String overDraft(int cardBalance) {
+
+        int amount = cardBalance + 1;
+        return Integer.toString(amount);
     }
 
     @Test
-    void shouldTransferMoneyBetweenOwnCardsV3() {
-        val loginPage = open("http://localhost:9999", LoginPageV3.class);
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
+    void shouldNotTransferOverDraft() {
+
+        Cards toCard = firstCard();
+        Cards fromCard = secondCard();
+        val transferPage = dashboardPage.transfer(toCard.getId());
+        String overDraft = overDraft(fromCard.getCardBalance());
+        transferPage.transferAmount(overDraft, fromCard.getCardNumber().strip());
+
+        val actualToCardBalance = dashboardPage.getCardBalance(toCard.getId());
+        val expectedToCardBalance = toCard.getCardBalance();
+        assertEquals(expectedToCardBalance, actualToCardBalance);
+
+        val actualFromCardBalance = dashboardPage.getCardBalance(fromCard.getId());
+        val expectedFromCardBalance = fromCard.getCardBalance();
+        assertEquals(expectedFromCardBalance, actualFromCardBalance);
     }
+
+
 }
-
